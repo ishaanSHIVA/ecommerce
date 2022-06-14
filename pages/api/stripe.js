b43eb445d893ad4a43b569f68abe4755fa20fc49
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-06-14 13:41:46 +0530

    complete

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
         }),
         mode: "payment",
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+        cancel_url: `${req.headers.origin}`,
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
